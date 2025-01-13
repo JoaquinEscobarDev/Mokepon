6cf0a5f48a8b86cffdbdbdc463a4a374fda7245b
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-01-13 09:44:21 -0300

    mejora de posicionamiento multijugador

--- js/mokepon.js
+++ js/mokepon.js
@@ -44,6 +44,8 @@ let ataqueJugador = [];
 
 let ataqueEnemigo = [];
 
+let mokeponesEnemigos = [];
+
 let opcionDeMokepones;
 
 let inputHipodoge;
@@ -529,11 +531,9 @@ function pintarCanvas() {
 
   enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y);
 
-  hipodogeEnemigo.pintarMokepon();
-
-  capipepoEnemigo.pintarMokepon();
-
-  ratigueyaEnemigo.pintarMokepon();
+  mokeponesEnemigos.forEach(function( mokepon ) {
+    mokepon.pintarMokepon();
+  });
 
   if (
     mascotaJugadorObjeto.velocidadX !== 0 ||
@@ -568,7 +568,7 @@ function enviarPosicion(x, y) {
         .then(function ({ enemigos }) {
           console.log(enemigos);
 
-          enemigos.forEach(function (enemigo) {
+          mokeponesEnemigos = enemigos.map(function (enemigo) {
             let mokeponEnemigo = null;
 
             const mokeponNombre = enemigo.mokepon.nombre || "";
@@ -600,7 +600,7 @@ function enviarPosicion(x, y) {
 
             mokeponEnemigo.y = enemigo.y;
 
-            mokeponEnemigo.pintarMokepon();
+            return mokeponEnemigo;
           });
         });
     }
